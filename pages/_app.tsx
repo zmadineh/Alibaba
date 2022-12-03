e2f8faf372fedab68002c22cb9e4e97c6c853f25
Amir partovi <a.partovi99@gmail.com>
2022-12-04 01:08:48 +0330

    test theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { CssBaseline } from '@mui/material'
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <ThemeProvider theme={theme}>
-    <CssBaseline/>
+    
     <Component {...pageProps} />
   </ThemeProvider>)
 }
