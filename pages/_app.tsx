1232d059cf6d1dc60cca564ba58da15fd790071e
Amir partovi <a.partovi99@gmail.com>
2022-12-04 00:46:02 +0330

    add assets , fonts ,Images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/system'
+import {theme} from '../Theme/Theme'
+import { CssBaseline } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Component {...pageProps} />
+  </ThemeProvider>)
 }
