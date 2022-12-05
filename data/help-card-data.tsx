f439e3f8452d6b70a283c52508bdb83e02ea5c80
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-05 18:19:44 +0330

    data for helpcart

---
+++ data/help-card-data.tsx
@@ -0,0 +1,9 @@
+import Darkhat from "../public/Assets/svg-helpcard/darkhast.svg"
+import Rahnama from "../public/Assets/svg-helpcard/rahnama.svg"
+import Poshtibani from "../public/Assets/svg-helpcard/poshtibani.svg"
+import Arrrow from "../public/Assets/svg-helpcard/arrow.svg"
+export const hlepCardData = [
+    { id: 1, icon: <Darkhat />, title: "درخواست استرداد", body: "سریع ترین راه برای کنسلی و لغو رزرو", body2: "سفرهای من", icon2: <Arrrow /> },
+    { id: 2, icon: <Rahnama />, title: "راهنمای سفر", body: "راهنمای خرید و استرداد قوانین و پرسش ها", body2: "مرکز پشتیبانی آنلاین", icon2: <Arrrow /> },
+    { id: 3, icon: <Poshtibani />, title: "درخواست پشتیبانی", body: "سریع ترین راه برای بررسی مشکلات شما", body2: "درخواست پشتیبانی", icon2: <Arrrow /> },
+]
