f4c50591302e07f385002c71f410d668e03a05cc
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-05 18:26:52 +0330

    put data in helpcard

--- components/home/help-card/HelpCard.tsx
+++ components/home/help-card/HelpCard.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { useTheme } from '@emotion/react';
 //material
 import Grid from '@mui/material/Grid';
-
 import { Typography } from '@mui/material';
-
-
+//data
+import { hlepCardData } from './../../../data/help-card-data';
 
 const HelpCard = () => {
     const theme = useTheme()
@@ -13,61 +12,25 @@ const HelpCard = () => {
         <Grid xs={12} container item justifyContent={"center"} alignItems={"center"} >
 
             <Grid container item xs={11} sx={theme => ({ border: "1px solid", borderColor: theme.palette.grey[200] })} bgcolor={"common.white"} borderRadius={5} p={2} gap={2} justifyContent={"center"} alignItems={"center"}>
-                {/* 1 */}
-                <Grid item xs={12} md={3.5} bgcolor={"red"} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2"}>
-                    <Grid item>
-                        svg
-                    </Grid>
-                    <Grid item className='taypo'>
+                {hlepCardData.map(item => (
+                    <Grid key={item.id} item xs={12} md={3.5} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2"}>
                         <Grid item>
-                            <Typography>1</Typography>
+                            {item.icon}
                         </Grid>
-                        <Grid item>
-                            <Typography>2</Typography>
-                        </Grid>
-                        <Grid item display={"flex"} gap={2} alignItems={"center"}>
-                            <Grid item> <Typography>3</Typography></Grid>
-                            <Grid item> svg</Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                {/* 2 */}
-                <Grid item xs={12} md={3.5} bgcolor={"red"} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2"}>
-                    <Grid item>
-                        svg
-                    </Grid>
-                    <Grid item className='taypo'>
-                        <Grid item>
-                            <Typography>1</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography>2</Typography>
-                        </Grid>
-                        <Grid item display={"flex"} gap={2} alignItems={"center"}>
-                            <Grid item> <Typography>3</Typography></Grid>
-                            <Grid item> svg</Grid>
+                        <Grid item className='taypo'>
+                            <Grid item>
+                                <Typography>{item.title}</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography>{item.body}</Typography>
+                            </Grid>
+                            <Grid item display={"flex"} gap={2} alignItems={"center"}>
+                                <Grid item> <Typography>{item.body2}</Typography></Grid>
+                                <Grid item>{item.icon2}</Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-                {/* 3 */}
-                <Grid item xs={12} md={3.5} bgcolor={"red"} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2"}>
-                    <Grid item>
-                        svg
-                    </Grid>
-                    <Grid item className='taypo'>
-                        <Grid item>
-                            <Typography>1</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography>2</Typography>
-                        </Grid>
-                        <Grid item display={"flex"} gap={2} alignItems={"center"}>
-                            <Grid item> <Typography>3</Typography></Grid>
-                            <Grid item> svg</Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-
+                ))}
             </Grid>
         </Grid>
     )
