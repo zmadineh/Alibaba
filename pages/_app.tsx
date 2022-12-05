e9ad8c93f44106ac839fb5a8873cd44e44bcc951
Amir partovi <a.partovi99@gmail.com>
2022-12-05 22:59:46 +0330

    aa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/system'
-import {theme} from '../theme/theme'
+import {theme} from '../themes/theme'
 import { CssBaseline } from '@mui/material'
 
 import rtlPlugin from 'stylis-plugin-rtl';
