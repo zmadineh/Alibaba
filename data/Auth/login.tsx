42bd2fdca58482ed2a838513524d89b67da9c1e7
Amir partovi <a.partovi99@gmail.com>
2022-12-05 11:09:11 +0330

    Auth added

---
+++ data/Auth/login.tsx
@@ -0,0 +1,23 @@
+interface LoginProps{
+    userName:string,
+    passWord:string
+}
+
+
+
+export const Login = ({userName , passWord}:LoginProps)=>{
+    return new Promise((resolve,reject)=>{
+        if (userName==='09123456789' && passWord==='123456'){
+            setTimeout(()=>{
+                resolve({
+                    name:'amir',
+                    age:24,
+                    country:'iran'
+                })
+            },5000)
+            
+        }else{
+            reject('invalid Username or Password')
+        }
+    })
+}
