37013f883c755ffb21de9fd74929039a893a9a48
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-05 21:52:12 +0330

    edit css helpercard components

--- components/home/help-card/HelpCard.tsx
+++ components/home/help-card/HelpCard.tsx
@@ -3,30 +3,32 @@ import { useTheme } from '@emotion/react';
 //material
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
+
+
 //data
 import { hlepCardData } from './../../../data/help-card-data';
+
 
 const HelpCard = () => {
     const theme = useTheme()
     return (
         <Grid xs={12} container item justifyContent={"center"} alignItems={"center"} >
-
-            <Grid container item xs={11} sx={theme => ({ border: "1px solid", borderColor: theme.palette.grey[200] })} bgcolor={"common.white"} borderRadius={5} p={2} gap={2} justifyContent={"center"} alignItems={"center"}>
+            <Grid container item xs={11} sx={theme => ({ border: "1px solid", borderColor: theme.palette.grey[200] })} bgcolor={"common.white"} borderRadius={5} p={2} gap={2} justifyContent={"center"} alignItems={"center"} boxShadow={1}>
                 {hlepCardData.map(item => (
-                    <Grid key={item.id} item xs={12} md={3.5} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2"}>
-                        <Grid item>
+                    <Grid key={item.id} item xs={12} md={3.5} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2px"}>
+                        <Grid item sx={theme => ({ color: theme.palette.info[300], backgroundColor: theme.palette.info[100] })} borderRadius={"28px"} py={"8px"} px={"9px"}>
                             {item.icon}
                         </Grid>
-                        <Grid item className='taypo'>
-                            <Grid item>
-                                <Typography>{item.title}</Typography>
+                        <Grid item className='taypo' display={"flex"} flexDirection={"column"} sx={{ textAlign: { sx: "right", md: "center" } }} gap={2}>
+                            <Grid item sx={theme => ({ color: theme.palette.grey[500] })} >
+                                <Typography sx={{ fontSize: 25, fontWeight: 700 }} >{item.title}</Typography>
                             </Grid>
-                            <Grid item>
-                                <Typography>{item.body}</Typography>
+                            <Grid item sx={theme => ({ color: theme.palette.grey[400] })}>
+                                <Typography sx={{ fontSize: 18, fontWeight: 600 }} >{item.body}</Typography>
                             </Grid>
-                            <Grid item display={"flex"} gap={2} alignItems={"center"}>
-                                <Grid item> <Typography>{item.body2}</Typography></Grid>
-                                <Grid item>{item.icon2}</Grid>
+                            <Grid item display={"flex"} gap={3} alignItems={"center"} sx={theme => ({ color: theme.palette.secondary.main, justifyContent: { xs: "flex-start", md: "center" }, cursor: "pointer" })}  >
+                                <Grid item> <Typography sx={{ fontSize: 18, fontWeight: 500 }}>{item.body2}</Typography></Grid>
+                                <Grid item sx={theme => ({ color: theme.palette.secondary.main })}>{item.icon2}</Grid>
                             </Grid>
                         </Grid>
                     </Grid>
--- data/help-card-data.tsx
+++ data/help-card-data.tsx
@@ -3,7 +3,7 @@ import Rahnama from "../public/Assets/svg-helpcard/rahnama.svg"
 import Poshtibani from "../public/Assets/svg-helpcard/poshtibani.svg"
 import Arrrow from "../public/Assets/svg-helpcard/arrow.svg"
 export const hlepCardData = [
-    { id: 1, icon: <Darkhat />, title: "درخواست استرداد", body: "سریع ترین راه برای کنسلی و لغو رزرو", body2: "سفرهای من", icon2: <Arrrow /> },
-    { id: 2, icon: <Rahnama />, title: "راهنمای سفر", body: "راهنمای خرید و استرداد قوانین و پرسش ها", body2: "مرکز پشتیبانی آنلاین", icon2: <Arrrow /> },
+    { id: 1, icon: <Darkhat />, title: "درخواست استرداد", body: "سریع ترین راه برای کنسلی و   لغو رزرو", body2: "سفرهای من", icon2: <Arrrow /> },
+    { id: 2, icon: <Rahnama />, title: "راهنمای سفر", body: "راهنمای خرید واسترداد قوانین و پرسش ها", body2: "مرکز پشتیبانی آنلاین", icon2: <Arrrow /> },
     { id: 3, icon: <Poshtibani />, title: "درخواست پشتیبانی", body: "سریع ترین راه برای بررسی مشکلات شما", body2: "درخواست پشتیبانی", icon2: <Arrrow /> },
 ]
