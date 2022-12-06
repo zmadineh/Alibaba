cbf21c872796c4480cff15583840e4be68d50591
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-06 09:15:41 +0330

    bug2

--- Theme/Theme.tsx
+++ Theme/Theme.tsx
@@ -11,7 +11,7 @@ export const theme = createTheme({
     typography: {
         fontFamily: 'Alibaba',
         h3: {
-            fontSize: 22,
+            fontSize: 20,
             fontWeight: 700,
             lineHeight: "21.7px",
 
