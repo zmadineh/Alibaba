69559a8530f47837cb51b6c1e7b67b3bd65619de
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-06 23:03:03 +0330

    fixbug brekpoint helpcard

--- components/home/help-card/HelpCard.tsx
+++ components/home/help-card/HelpCard.tsx
@@ -14,7 +14,7 @@ const HelpCard = () => {
     const theme = useTheme()
     return (
         <Grid xs={12} container item justifyContent={"center"} alignItems={"center"} >
-            <Grid container item xs={11} sx={{ border: "1px solid", borderColor: 'grey.200' }} bgcolor={"common.white"} borderRadius={5} p={2} gap={2} justifyContent={"center"} alignItems={"center"} boxShadow={1}>
+            <Grid container item sx={{ border: 2, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', bgcolor: 'common.white', maxWidth: "1000px" }} bgcolor={"common.white"} p={2} gap={2} justifyContent={"center"} alignItems={"center"} boxShadow={1}>
                 {hlepCardData.map(item => (
                     <Grid key={item.id} item xs={12} md={3.5} display={"flex"} gap={2} alignItems={"center"} sx={{ flexDirection: { xs: "row", md: "column" } }} p={"2px"}>
                         <Grid item sx={{ color: 'info.300', backgroundColor: 'info.100' }} borderRadius={"28px"} py={"8px"} px={"9px"}>
