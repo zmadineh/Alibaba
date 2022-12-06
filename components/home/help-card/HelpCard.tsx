279f947e5fc382479fd7750cbbdf9caf0a02f945
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-06 20:37:44 +0330

    edit color helpcard

--- components/home/help-card/HelpCard.tsx
+++ components/home/help-card/HelpCard.tsx
@@ -21,15 +21,15 @@ const HelpCard = () => {
                             {item.icon}
                         </Grid>
                         <Grid item className='taypo' display={"flex"} flexDirection={"column"} sx={{ textAlign: { sx: "right", md: "center" } }} gap={1}>
-                            <Grid item sx={{ color: 'grey.500' }} >
-                                <Typography variant="h6" sx={{ fontWeight: 600 }} > {item.title}</Typography>
+                            <Grid item sx={{ color: 'grey.600' }} >
+                                <Typography variant="h6" sx={{ fontWeight: 600, fontSize: 16 }} > {item.title}</Typography>
                             </Grid>
-                            <Grid item sx={{ color: 'grey.400' }}>
-                                <Typography variant='h6' sx={{ fontWeight: 400 }}>{item.body}</Typography>
+                            <Grid item sx={{ color: 'grey.500' }}>
+                                <Typography variant='h6' sx={{ fontWeight: 400, fontSize: 14 }}>{item.body}</Typography>
                             </Grid>
                             <Grid item  >
                                 <Button sx={{ color: "secondary.300", borderRadius: "10px", "&:hover": { backgroundColor: "secondary.100" } }}>
-                                    <Grid item> <Typography variant='h6' sx={{ marginBottom: "14px", fontWeight: 400, color: 'secondary.main' }}  >{item.body2}</Typography></Grid>
+                                    <Grid item> <Typography variant='h6' sx={{ marginBottom: "14px", fontWeight: 400, color: 'secondary.main', fontSize: 14 }}  >{item.body2}</Typography></Grid>
                                     <Grid item sx={{ color: 'secondary.main' }}> {item.icon2}</Grid>
                                 </Button>
 
