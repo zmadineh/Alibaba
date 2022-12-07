7cddbf80c1f18f9b052dee3f5cb68ad0c0c97d06
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-08 01:11:48 +0330

    fixbug download card mobile

--- components/home/download-card/DawnloadCardMobile.tsx
+++ components/home/download-card/DawnloadCardMobile.tsx
@@ -4,16 +4,22 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 import Link from 'next/link';
-
+import Image from 'next/image';
+import Download from "../../../public/Assets/Images/download-card/download.jpg"
+import { grey } from '@mui/material/colors';
+import { display } from '@mui/system';
 
 
 const DawnloadCardMobile = () => {
     return (
-        <Grid item xs={12} sx={{ display: { xs: "flex", sm: "none" }, marginLeft: "5px", marginTop: "5px" }} justifyContent={"center"} alignItems={"center"} gap={1} >
-            <Grid item display={"flex"} flexDirection={"column"} gap={1}>
-                <Grid item><Typography variant='h6' sx={{ fontSize: 16, fontWeight: 700 }}>اپلیکیشن علی بابا</Typography></Grid>
-                <Grid item><Typography sx={{ color: "grey.500", fontSize: 12, fontWeight: 700 }}>همه ی سفرها در جیب شماست</Typography></Grid>
-                <Grid xs={8} item ><Link href={"/"}><Button variant='Button2' ><Typography sx={{ fontWeight: 500, fontSize: 12 }}>راهنمای نصب</Typography></Button></Link></Grid>
+        <Grid xs={12} item sx={{ display: { xs: "flex", md: "none" } }} height={175} alignItems={"flex-end"} >
+            <Grid item xs={6} display={"flex"} flexDirection={"column"} justifyContent={"space-evenly"} height={175} p={1}>
+                <Grid item ><Typography variant='h6' sx={{ fontSize: "1rem", fontWeight: "700" }}>اپلیکیشن<br />علی بابا</Typography></Grid>
+                <Grid item><Typography sx={{ fontSize: ".75rem", fontWeight: "400", color: "grey.500" }}> همه سفرها در جیب شماست</Typography></Grid>
+                <Grid item width={110}><Link href={"/"}><Button variant='Button2'  ><Typography sx={{ fontSize: ".75rem", fontWeight: "600" }} >راهنمای نصب</Typography ></Button></Link></Grid>
+            </Grid>
+            <Grid item xs={6} height={120}  >
+                <Image src={Download} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
             </Grid>
         </Grid>
     )
