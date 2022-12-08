1476c5dd8a4f5f73a1f7fbf1b83adf2f96853e7d
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-08 13:56:08 +0330

    fixbug downloadCardPc

--- components/home/download-card/DownloadCardPc.tsx
+++ components/home/download-card/DownloadCardPc.tsx
@@ -7,33 +7,43 @@ import Image from 'next/image';
 import Arrow from "../../../public/Assets/svg-helpcard/arrow.svg"
 import Android from "../../../public/Assets/Images/download-card/android.png"
 import Apple from "../../../public/Assets/Images/download-card/apple.png"
+import Scan from "../../../public/Assets/Images/download-card/scan.jpg"
+import Download from "../../../public/Assets/Images/download-card/download.jpg"
 const DownloadCardPc = () => {
     return (
-        <Grid item sm={12} md={3} sx={{ display: { xs: "none", sm: "flex" }, alignItems: { sm: "center", md: "flex-start" } }} flexDirection={"column"} alignItems={"flex-start"} justifyContent={"center"} gap={3} >
-            <Grid item sx={{ display: { xs: "none", sm: "flex" } }} flexDirection={"column"}>
-                <Grid item><Typography variant='h6' sx={{ fontSize: 16, fontWeight: 700 }}>اپلیکیشن علی بابا</Typography></Grid>
-                <Grid item>  <Typography variant='h6' sx={{ fontSize: 18, fontWeight: "medium" }}>سریع تر و مطمئن تر به سفر بروید </Typography></Grid>
+        <Grid item md={12} sx={{ display: { xs: "none", md: "flex" }, flexDirection: { md: "column", lg: "row" }, justifyContent: { md: "center", lg: "space-between" } }} alignItems={"center"} gap={2}>
+            <Grid item md={12} display={"flex"} justifyContent={"flex-end"} height={187} sx={{ marginLeft: 6, marginTop: 1, marginRight: 2, width: { md: 146, lg: 330 } }}>
+                <Image src={Scan} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
             </Grid>
-            <Grid item sx={{ display: { xs: "none", sm: "flex" } }} alignItems={"flex-start"} flexDirection={"column"}>
+            <Grid item md={12} display={'flex'} flexDirection={"column"} gap={2} >
+                <Grid item>
+                    <Typography variant='h4' sx={{ fontSize: 20, fontWeight: 700 }}>اپلیکیشن علی بابا</Typography>
+                    <Typography sx={{ fontSize: 18 }}>سریع تر و مطمئن تر به سفر بروید</Typography></Grid>
                 <Grid item>
                     <Button sx={{ color: "secondary.300", borderRadius: "10px", "&:hover": { backgroundColor: "secondary.100" } }}>
-                        <Grid item> <Typography variant='h6' sx={{ fontWeight: 400, color: 'secondary.main', fontSize: 14, textAlign: "right", marginBottom: "10px" }}  >مشاهده لینک های دانلود</Typography></Grid>
+                        <Grid item> <Typography variant='h6' sx={{ marginBottom: "14px", fontWeight: 400, color: 'secondary.main', fontSize: 16 }}  >مشاهده لینک های دانلود</Typography></Grid>
                         <Grid item sx={{ color: 'secondary.main' }}> <Arrow /></Grid>
                     </Button>
                 </Grid>
-                <Grid item display={"flex"} alignItems={"flex-end"}>
-                    <Grid item width={20} height={20}>
-                        <Image src={Android} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
-                    </Grid>
-                    <Grid item width={20} height={20}>
-                        <Image src={Apple} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
-                    </Grid>
-                    <Grid item >
-                        <Typography sx={{ fontSize: 12, color: "grey.400" }}>قابلیت نصب روی AndroidوiOS</Typography>
+                <Grid item md={12} display={"flex"} justifyContent={"flex-start"}  >
+                    <Grid item display={"flex"} alignItems={"flex-end"}  >
+                        <Grid item width={20} height={20}>
+                            <Image src={Apple} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
+                        </Grid>
+                        <Grid item width={20} height={20}>
+                            <Image src={Android} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
+                        </Grid>
+                        <Grid item>
+                            <Typography sx={{ fontSize: 13, fontWeight: 400, color: "grey.400" }}> قابلیت نصب روی Android و iOS </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
+            <Grid item sx={{ width: { md: 400, lg: 700 }, marginRight: 5 }} height={305} p={1} >
+                <Image src={Download} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
+            </Grid>
         </Grid>
+
     )
 }
 
