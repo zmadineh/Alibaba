8de4ff37bbfef4e4390554eff06ea5d8e3815bf3
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-08 15:52:10 +0330

    fixbug width scanimage

--- components/home/download-card/DownloadCardPc.tsx
+++ components/home/download-card/DownloadCardPc.tsx
@@ -12,7 +12,7 @@ import Download from "../../../public/Assets/Images/download-card/download.jpg"
 const DownloadCardPc = () => {
     return (
         <Grid item md={12} sx={{ display: { xs: "none", md: "flex" }, flexDirection: { md: "column", lg: "row" }, justifyContent: { md: "center", lg: "space-between" } }} alignItems={"center"} gap={2}>
-            <Grid item md={12} display={"flex"} justifyContent={"flex-end"} height={187} sx={{ marginLeft: 6, marginTop: 1, marginRight: 2, width: { md: 146, lg: 310 } }}>
+            <Grid item md={12} display={"flex"} justifyContent={"flex-end"} height={187} sx={{ marginLeft: 6, marginTop: 1, marginRight: 2, width: { md: 146, lg: 300 } }}>
                 <Image src={Scan} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
             </Grid>
             <Grid item md={12} display={'flex'} flexDirection={"column"} gap={2} >
