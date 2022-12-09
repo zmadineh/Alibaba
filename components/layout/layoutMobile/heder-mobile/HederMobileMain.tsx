d7abc8b662e6cdd76288d33c2c1c973363db7639
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-09 20:05:39 +0330

    fixbug border

--- components/layout/layoutMobile/heder-mobile/HederMobileMain.tsx
+++ components/layout/layoutMobile/heder-mobile/HederMobileMain.tsx
@@ -10,15 +10,15 @@ import { listType } from './../../../../data/listOption';
 
 function HederMobileMain({ item }: { item: listType }): JSX.Element {
     return (
-        <Grid key={item.id} xs={12} item display={"flex"} justifyContent={"flex-start"} alignItems={"center"} sx={{ border: "1px solid", borderColor: 'grey.200' }}>
+        <Grid key={item.id} xs={12} item display={"flex"} justifyContent={"flex-start"} alignItems={"center"} sx={{ border: "0.1px solid", borderColor: 'divider' }}>
             <Button variant='Button1' >
                 <Link href={item.href} >
                     <Grid display={"flex"} sx={{ paddingRight: 2, color: 'grey.700' }} gap={1} alignItems={"center"}>
                         <Grid item >
                             {item.icon}
                         </Grid>
-                        <Grid item sx={{ marginBottom: "5px" }}>
-                            <Typography variant='h6' sx={{ textDecoriarion: "none solid grey.700" }}><strong>{item.title}</strong></Typography>
+                        <Grid item sx={{ marginBottom: "8px" }}>
+                            <Typography variant='h6' sx={{ textDecoriarion: "none solid grey.700", fontSize: 16 }}><strong>{item.title}</strong></Typography>
                         </Grid>
                     </Grid>
                 </Link>
