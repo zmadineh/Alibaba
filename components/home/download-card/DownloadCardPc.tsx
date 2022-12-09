967958679467accbc0d06aafb7ef612021e039c4
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-09 17:46:34 +0330

    fixbug width

--- components/home/download-card/DownloadCardPc.tsx
+++ components/home/download-card/DownloadCardPc.tsx
@@ -12,10 +12,10 @@ import Download from "../../../public/Assets/Images/download-card/download.jpg"
 const DownloadCardPc = () => {
     return (
         <Grid item md={12} sx={{ display: { xs: "none", md: "flex" }, flexDirection: { md: "column", lg: "row" }, justifyContent: { md: "center", lg: "space-between" } }} alignItems={"center"} gap={2}>
-            <Grid item md={12} display={"flex"} justifyContent={"flex-end"} height={187} sx={{ marginLeft: 6, marginTop: 1, marginRight: 2, width: { md: 146, lg: 150 } }}>
-                <Image src={Scan} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
+            <Grid item md={12} lg={4} display={"flex"} justifyContent={"flex-end"} height={187} sx={{ marginLeft: 4, marginTop: 1 }}>
+                <Image src={Scan} alt="علی بابا" style={{ width: 148, height: "100%" }} />
             </Grid>
-            <Grid item md={12} display={'flex'} flexDirection={"column"} gap={2} >
+            <Grid item md={12} lg={4} display={'flex'} flexDirection={"column"} gap={2} >
                 <Grid item>
                     <Typography variant='h6' sx={{ fontSize: "1.25rem", fontWeight: 700 }}>اپلیکیشن علی بابا</Typography>
                     <Typography sx={{ fontSize: "1.125rem", }}>سریع تر و مطمئن تر به سفر بروید</Typography></Grid>
@@ -39,8 +39,8 @@ const DownloadCardPc = () => {
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item sx={{ width: { md: 400, lg: 410 }, marginRight: 5 }} height={305} p={1} >
-                <Image src={Download} alt="علی بابا" style={{ width: "100%", height: "100%" }} />
+            <Grid item md={12} lg={4} sx={{ marginRight: 5 }} height={300} p={1} >
+                <Image src={Download} alt="علی بابا" style={{ width: 369, height: "100%" }} />
             </Grid>
         </Grid>
 
