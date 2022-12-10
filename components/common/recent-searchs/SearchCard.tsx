0db0f012f01d11d9aba171acb5f08ab0d33a372c
Zahra Mdienh <madinehzahra76@gmail.com>
2022-12-10 22:02:45 +0330

    feat: add new section to recent card

--- components/common/recent-searchs/SearchCard.tsx
+++ components/common/recent-searchs/SearchCard.tsx
@@ -9,6 +9,7 @@ import {useTheme} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import AirlineSeatReclineExtraOutlinedIcon from '@mui/icons-material/AirlineSeatReclineExtraOutlined';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import {passengersCount} from "../../../model/passengerCount.type";
 
@@ -33,40 +34,45 @@ export default function SearchCard({origin, destination, oneWayRoad, departureDa
         >
             <CardActionArea>
                 <CardContent>
-                    <Grid container gap={1}>
-                        <Typography gutterBottom variant="body1" noWrap textOverflow={'ellipsis'}>
-                            {origin}
-                        </Typography>
-                        <ArrowBackIosIcon fontSize={"small"}/>
-                        <Typography gutterBottom variant="body1" noWrap textOverflow={'ellipsis'}>
-                            {destination}
-                        </Typography>
-                    </Grid>
-
-
-                    <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
-                        {oneWayRoad ? 'سفر یک طرفه' : 'رفت و برگشت'}
-                    </Typography>
-
-                    <Grid container gap={1}>
-                        <CalendarMonthOutlinedIcon />
-                        <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
-                            {departureDate}
-                        </Typography>
-                        {!oneWayRoad &&
-                            <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
-                                {returnDate}
+                    <Grid container flexDirection={"column"} gap={1}>
+                        <Grid container gap={1} alignItems={"center"}>
+                            <Typography gutterBottom variant="body1" noWrap textOverflow={'ellipsis'}>
+                                {origin}
                             </Typography>
-                        }
-                    </Grid>
+                            <KeyboardArrowLeftIcon fontSize={"large"}/>
+                            <Typography gutterBottom variant="body1" noWrap textOverflow={'ellipsis'}>
+                                {destination}
+                            </Typography>
+                        </Grid>
 
-                    <Grid container gap={1}>
-                        <AirlineSeatReclineExtraOutlinedIcon />
-                        <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
-                            {`${passengerCount.adult} یزرگسال` + '، ' + `${passengerCount.child} کودک` + '، ' + `${passengerCount.baby} نوزاد` }
+
+                        <Typography variant="body2" color={'grey.700'} noWrap textOverflow={'ellipsis'}>
+                            {oneWayRoad ? 'سفر یک طرفه' : 'رفت و برگشت'}
                         </Typography>
-                    </Grid>
 
+                        <Grid container gap={1} color={'grey.700'}>
+                            <CalendarMonthOutlinedIcon />
+                            <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
+                                {departureDate}
+                            </Typography>
+                            {!oneWayRoad &&
+                                <KeyboardArrowLeftIcon fontSize={"small"}/>
+                            }
+                            {!oneWayRoad &&
+                                <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
+                                    {returnDate}
+                                </Typography>
+                            }
+                        </Grid>
+
+
+                        <Grid container gap={1} color={'grey.700'}>
+                            <AirlineSeatReclineExtraOutlinedIcon />
+                            <Typography variant="body2" noWrap textOverflow={'ellipsis'}>
+                                {`${passengerCount.adult} بزرگسال`}{passengerCount.child > 0 ? ( ' ،' + `${passengerCount.child} کودک` ): null}{passengerCount.baby > 0 ? (' ،' + `${passengerCount.baby} نوزاد`): null }
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </CardActionArea>
         </Card>
--- components/common/recent-searchs/SearchCardContainer.tsx
+++ components/common/recent-searchs/SearchCardContainer.tsx
@@ -12,7 +12,7 @@ export default function SearchCardContainer() {
             destination: 'اصفهان',
             oneWayRoad: true,
             departureDate: ' 29 آذر',
-            returnDate: ' 1 دی',
+            returnDate: '',
             passengerCount: {adult: 1, child: 0, baby: 0}
         },
         {
@@ -20,7 +20,7 @@ export default function SearchCardContainer() {
             destination: 'شیراز',
             oneWayRoad: false,
             departureDate: ' 29 آذر',
-            returnDate: '',
+            returnDate: '1 دی',
             passengerCount: {adult: 1, child: 2, baby: 0}
         }
     ]
@@ -29,10 +29,12 @@ export default function SearchCardContainer() {
     return (
         <Grid container my={3} width={'1000px'} justifyContent={"center"} alignItems={"center"}>
             <Grid width={'100%'}>
-                <Grid item container justifyContent={"space-between"}>
+
+                <Grid item container justifyContent={"space-between"} color={'grey.800'}>
                     <Button variant={"text"}><Typography variant={'body1'}>جستجوهای اخیر</Typography></Button>
                     <Button variant={"text"}><Typography variant={'body2'}>همه جستجوها</Typography></Button>
                 </Grid>
+
                 <Grid container gap={2} mt={4}>
                     {input.map((data , index) => (
                         <SearchCard key={index} {...data}/>
