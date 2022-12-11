554b70ae1fe14030af9d81dde0a1dc509c96e421
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-11 11:06:42 +0330

    fixbug width

--- components/home/help-card/HelpCard.tsx
+++ components/home/help-card/HelpCard.tsx
@@ -13,7 +13,7 @@ const HelpCard = (): JSX.Element => {
 
     return (
         <Grid xs={12} container justifyContent={"center"} alignItems={"center"} marginBottom={3} >
-            <Grid container item sx={{ border: 2, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', bgcolor: 'common.white', maxWidth: "1000px" }} bgcolor={"common.white"} p={2} gap={2} justifyContent={"center"} alignItems={"center"}>
+            <Grid container item sx={{ border: 2, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', bgcolor: 'common.white', maxWidth: "1200px" }} bgcolor={"common.white"} p={2} gap={2} justifyContent={"center"} alignItems={"center"}>
                 {hlepCardData.map(item => (<HelpCardItem item={item} key={item.id} />))}
             </Grid>
         </Grid >
