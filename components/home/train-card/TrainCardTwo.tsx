5d4d905f4feeea0bfe878269221c693a5e88b3e3
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-11 21:24:25 +0330

    fixbug TtainCard

--- components/home/train-card/TrainCardTwo.tsx
+++ components/home/train-card/TrainCardTwo.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
-import { trainCardDataTwo } from "../../../data/traincards/train-card-data-two"
+// material ui
 import Grid from '@mui/material/Grid';
+// Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Scrollbar } from "swiper";
+// data
+import { trainCardDataTwo } from './../../../data/traincards/train-card-data-two';
 import TrainItem from './TrainItem';
-
-
 const TrainCardTwo = () => {
     return (
         <Grid item container xs={12} justifyContent={"center"} alignItems={"center"} marginBottom={3}>
             <Grid item container xs={12} sx={{ maxWidth: { md: '1200px' }, bgcolor: 'common.white' }} justifyContent={"center"} >
                 <Grid container xs={12} item flexDirection={"row"} justifyContent={"space-between"} sx={{ overflowX: "auto" }} flexShrink={0}>
+                    {/* pc */}
                     {trainCardDataTwo.map(item => (
-                        <Grid item xs={2.9} key={item.id} display={"flex"} borderRadius={"0.5rem"} flex-direction={"column"} overflow={"hidden"} flexWrap={"wrap"} sx={{ border: 1, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', }} >
+                        <Grid item md={2.9} key={item.id} borderRadius={"0.5rem"} flex-direction={"column"} overflow={"hidden"} flexWrap={"wrap"} sx={{ border: 1, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', display: { xs: "none", md: "flex" } }} height={550}>
                             <TrainItem item={item} />
-                        </Grid>))}
+                        </Grid>)
+                    )}
+                </Grid>
+                {/* mobile */}
+                <Grid item container xs={12} justifyContent={"center"} p={1}>
+                    <Grid item width={400}>
+                        <Swiper
+                            scrollbar={{
+                                hide: true,
+                            }}
+                            modules={[Scrollbar]}  >
+                            {trainCardDataTwo.map(item => (
+                                <SwiperSlide key={item.id} ><Grid overflow={"hidden"} xs={12} sx={{ border: 1, borderRadius: '10px 10px 10px 10px', borderColor: 'divider', display: { xs: "flex", md: "none" } }} justifyContent={"center"} height={550}>
+                                    <TrainItem item={item} /></Grid></SwiperSlide>
+                            ))}
+
+                        </Swiper>
+                    </Grid>
                 </Grid>
 
             </Grid>
--- components/home/train-card/TrainItem.tsx
+++ components/home/train-card/TrainItem.tsx
@@ -3,25 +3,16 @@ import Image from 'next/image';
 //material ui
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-
-
 //data
-
-
 import Link from 'next/link';
 import { trainCardType } from '../../../model/train.type';
-
-
-
-
-
 
 const TrainItem = ({ item }: { item: trainCardType }) => {
 
     return (
-        <Grid item >
-            <Grid item display={"flex"} justifyContent={"flex-start"} alignItems={"flex-end"} position={"relative"} height={180} width={390}>
-                <Image src={item.image} alt="قطار" width={390} height={180} />
+        <Grid item width={"100%"}>
+            <Grid item display={"flex"} justifyContent={"flex-start"} alignItems={"flex-end"} position={"relative"} height={200} width={400}>
+                <Image src={item.image} alt="قطار" width={400} height={200} />
                 <Grid position={"absolute"} item width={"100%"} sx={{ backgroundImage: "linear-gradient(to top,black,rgba(0,0,0,0))", color: "common.white", }}><Typography sx={{ fontWeight: 700, px: 1, fontSize: 16 }} variant='h6'>{item.title}</Typography></Grid>
             </Grid>
             <Grid item sx={{ padding: "1rem" }} height={300} width={"100%"} justifyContent={"center"}>
@@ -31,8 +22,6 @@ const TrainItem = ({ item }: { item: trainCardType }) => {
                 </Typography>
                 <Typography sx={{ fontSize: { xs: "./875rem", lg: 14 }, lineHeight: 2.3 }}>{item.body2}</Typography>
             </Grid>
-
-
         </Grid>
     )
 }
