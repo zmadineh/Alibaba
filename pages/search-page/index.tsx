5c1d93270ce64863cdbb838cf7f9d2f57f98ded2
Zahra Mdienh <madinehzahra76@gmail.com>
2022-12-13 19:19:12 +0330

    feat: add desktop footer section

--- pages/search-page/index.tsx
+++ pages/search-page/index.tsx
@@ -11,37 +11,56 @@ export default function SearchPage() {
     const mobileMatch = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <Grid container flexDirection={"column"} minHeight={'100vh'} alignItems={"center"}>
+        <Grid container flexDirection={"column"} alignItems={"center"}>
             <Grid item container bgcolor={"gray"} height={`${headerHeight}px`} position={"fixed"} top={0}>
                 HEADER
             </Grid>
 
-            <Grid item container width={'100%'} height={'100%'}
+            <Grid item container
+                  flexDirection={"column"}
+                  width={'100%'}
                   sx={{maxWidth: {lg: '1200px', sm: '100%'}}}
                   mt={{xs: `${headerHeight}px`, sm: `${headerHeight+10}px`}}
-                  justifyContent={"center"}
+                  alignItems={"center"}
             >
-                {!mobileMatch &&
-                    <Grid item xs={12} sm={4} bgcolor={'blue'}>
-                        SIDE FILTER
-                    </Grid>
-                }
-
-                <Grid item container gap={1} mx={{xs: 0, sm: 1}} flexDirection={"column"} xs={12} sm={7}>
-
-                    <Grid item height={'100px'} bgcolor={'yellow'}> date </Grid>
-
+                <Grid item container width={'100%'} height={'100%'}
+                      justifyContent={"center"}
+                >
                     {!mobileMatch &&
-                        <Grid item height={'100px'} bgcolor={'red'}> order filter</Grid>
+                        <Grid item xs={12} sm={4} bgcolor={'blue'}>
+                            SIDE FILTER
+                        </Grid>
                     }
 
-                    <Grid item height={'800px'} bgcolor={'green'}>
-                        tickets
-                    </Grid>
+                    <Grid item container gap={1} pl={{xs: 0, sm: 1}} flexDirection={"column"} xs={12} sm={8}>
+                        <Grid item height={'100px'} bgcolor={'yellow'}> date </Grid>
 
+                        {!mobileMatch &&
+                            <Grid item height={'100px'} bgcolor={'red'}> order filter</Grid>
+                        }
+
+                        <Grid item height={'800px'} bgcolor={'green'}>
+                            tickets
+                        </Grid>
+                    </Grid>
                 </Grid>
+
+                {!mobileMatch &&
+                    <Grid item container width={'100%'} height={'400px'}
+                          px={{xs: 0, sm: 1}} mt={1}
+                          justifyContent={"center"} bgcolor={'purple'}
+                    >
+                        common questions
+                    </Grid>
+                }
             </Grid>
 
+            {/* desktop footer */}
+            <Grid item container bgcolor={"gray"} minHeight={'60px'}>
+                footer
+            </Grid>
+
+            {/* mobile footer */}
             {mobileMatch &&
                 <Grid item container bgcolor={"gray"} height={'60px'} position={"fixed"} bottom={0}>
                     <Grid item display={"flex"} xs={6}>
