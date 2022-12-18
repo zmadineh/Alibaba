ad3fd2a599356c535bf23ab06f39e6b83ec48ff5
Amir partovi <a.partovi99@gmail.com>
2022-12-18 16:48:00 +0330

    primary tabs

--- components/common/date-filter/DateFilter.tsx
+++ components/common/date-filter/DateFilter.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
 type Props = {}
 
 const DateFilter = (props: Props) => {
+
+    const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>DateFilter</div>
+    <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        allowScrollButtonsMobile
+        aria-label="scrollable force tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
+    </Box>
   )
 }
 
