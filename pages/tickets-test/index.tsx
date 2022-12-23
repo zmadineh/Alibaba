180d34ac369b57f669f8ad1c50e6fbe7f3a282e0
nima babaei <nimani1378@gmail.com>
2022-12-23 20:11:41 +0330

    refactor ticket page for see changes

--- pages/tickets-test/index.tsx
+++ pages/tickets-test/index.tsx
@@ -1,13 +1,39 @@
 import { Grid } from "@mui/material";
 import BusTicket from "../../components/ticket_cards/BusTicket";
 import TrainTicket from "../../components/ticket_cards/TrainTicket";
+import TicketComponent from "../../components/ticket_cards/TicketComponent";
+import { getTicket } from "../../data/tickets_data/DataTickets";
+import { filterd_TripData } from "../../data/tickets_data/DataTickets";
 
 export default function Home() {
+    let filteredData : filterd_TripData[] = [
+        {
+            departure_date: new Date(1401, 3, 24, 18, 26),
+            company_name: 'چابک سواران',
+            company_Score: 4.4,
+            company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
+            trip_des: ['ماهانVIP(مانیتورشخصی)'],
+            start_point_city: 'اصفهان',
+            destination_city: 'تهران',
+            Remaining_seats: 10,
+            price: 1220000,
+        },
+        {
+            departure_date: new Date(1401, 5, 3, 6, 2),
+            company_name: 'چابک سواران',
+            company_Score: 4.4,
+            company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
+            trip_des: ['ماهانVIP(مانیتورشخصی)','اکونومی'],
+            start_point_city: 'اصفهان',
+            destination_city: 'تهران',
+            Remaining_seats: 10,
+            price: 1320000
+        },
+    ]
     return (
         <Grid display={'flex'} direction={'row'} justifyContent={'center'} alignItems='center' width={'100vw'} height={'100vh'}>
             <Grid width={'1000px'} padding='10px'>
-                <BusTicket />
-                <TrainTicket/>
+                <TicketComponent filteredData={filteredData} tripType={3}/>
             </Grid>
         </Grid>
     )
