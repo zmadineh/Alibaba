8bdea489fe1e8da806ca3a8968fa28285a22cd35
nima babaei <nimani1378@gmail.com>
2022-12-28 14:37:37 +0330

    refactor and clean data types of trips

--- data/tickets_data/DataTickets.ts
+++ data/tickets_data/DataTickets.ts
@@ -1,41 +1,4 @@
-import { companies_list } from "./CompaniesData";
-
-interface returnType {
-    filteredData: filterd_TripData[],
-   
-}
-export function getTicket(category: string, date: Date, travelerCount: number): filterd_TripData[] {
-    let filteredData: filterd_TripData[];
-    switch (category) {
-        case 'bus':
-
-            filteredData = BusTicket.filter(item => (item.Remaining_seats >= travelerCount) && (item.departure_date.getDate() === date.getDate()) && (item.departure_date.getFullYear() === date.getFullYear()) && (item.departure_date.getMonth() === date.getMonth()));
-            return filteredData
-
-        // case 'train':
-        //     const filteredData: filterd_TripData[] = BusTicket.filter(item => (item.Remaining_seats >= travelerCount) && (item.departure_date.getDate() === date.getDate()) && (item.departure_date.getFullYear() === date.getFullYear()) && (item.departure_date.getMonth() === date.getMonth()));
-        //     const tripType = 3;
-        //     return {filteredData,tripType}
-        default:
-            filteredData = [];
-            return filteredData
-    }
-}
-
-export interface BusTicket_type {
-    ticketDate: Date,
-    company_name: string,
-    company_Score: number,
-    company_image: string,
-    Bus_des: string,
-    start: string,
-    start_terminal: string,
-    destination: string,
-    destination_terminal: string,
-    Remaining_seats: number,
-    price: number
-}
-interface Trip_type {
+export interface Trip_type {
     id: number,
     transport_type_id: number,
     departure_date: Date,
@@ -46,13 +9,13 @@ interface Trip_type {
     destination_city_id: number,
     //terminal_id:number
     round_trip: boolean,
-    Remaining_seats: number,
+    remaining_seats: number,
     price: number,
-    shopping_type: string | null,
+    shopping_type?: string,
     trip_des: string[]
 }
-
 export interface filterd_TripData {
+    transport_company_id: number,
     company_name: string,
     company_Score?: number,
     company_image: string,
@@ -60,10 +23,15 @@ export interface filterd_TripData {
     receive_date?: Date,
     start_point_city: string,
     destination_city: string,
-    Remaining_seats: number,
+    remaining_seats: number,
     price: number,
     shopping_type?: string,
-    trip_des: string[]
+    trip_des: string[],
+    return_date?: Date,
+    return_receive_date?: Date,
+    return_transport_company_name?: string,
+    return_transport_company_image?: string,
+    return_transport_company_score?: number,
 }
 
 export interface ReturnTrip_type extends Trip_type {
@@ -71,86 +39,3 @@ export interface ReturnTrip_type extends Trip_type {
     return_receive_date: Date,
     return_transport_company_id: number
 }
-
-
-export interface trainTicket_type {
-    ticketDate: Date,
-    company_name: string,
-    company_image: string,
-    train_des: string,
-    train_room_des: string,
-    start: string,
-    destination: string,
-    destination_terminal: string,
-    Remaining_seats: number,
-    price: number,
-
-}
-let BusTicket: filterd_TripData[] = [
-    {
-        departure_date: new Date(1401, 3, 24, 18, 26),
-        company_name: 'چابک سواران',
-        company_Score: 4.4,
-        company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-        trip_des: ['ماهانVIP(مانیتورشخصی)'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 10,
-        price: 1220000,
-    },
-    {
-        departure_date: new Date(1401, 5, 3, 6, 2),
-        company_name: 'چابک سواران',
-        company_Score: 4.4,
-        company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-        trip_des: ['ماهانVIP(مانیتورشخصی)'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 10,
-        price: 1220000
-    },
-    {
-        departure_date: new Date(1401, 3, 21, 20, 30),
-        company_name: 'همسفر سواران',
-        company_Score: 4.4,
-        company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-        trip_des: ['ماهانVIP(مانیتورشخصی)'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 4,
-        price: 1220000
-    }
-]
-
-let TarinTicket: filterd_TripData[] = [
-    {
-        departure_date: new Date(1401, 3, 24, 18, 26),
-        company_name: 'ريل ترابر سبا',
-        company_image: 'https://cdn.alibaba.ir/static/img/train/train_15.png',
-        trip_des: ['کوپه‌ای 4 نفره'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 10,
-        price: 1220000
-    },
-    {
-        departure_date: new Date(1401, 5, 3, 6, 2),
-        company_name: 'چابک سواران',
-        company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-        trip_des: ['کوپه‌ای 4 نفره'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 10,
-        price: 1220000
-    },
-    {
-        departure_date: new Date(1401, 3, 21, 20, 30),
-        company_name: 'همسفر سواران',
-        company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-        trip_des: ['کوپه‌ای 4 نفره'],
-        start_point_city: 'اصفهان',
-        destination_city: 'تهران',
-        Remaining_seats: 4,
-        price: 1220000
-    }
-]
