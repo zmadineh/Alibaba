99f0eda3bf011659e18c5ffd0f89720606fb4ceb
nima babaei <nimani1378@gmail.com>
2022-12-28 14:36:15 +0330

    refactor to see if async func does work

--- pages/tickets-test/index.tsx
+++ pages/tickets-test/index.tsx
@@ -2,39 +2,30 @@ import { Grid } from "@mui/material";
 import BusTicket from "../../components/ticket_cards/BusTicket";
 import TrainTicket from "../../components/ticket_cards/TrainTicket";
 import TicketContainer from "../../components/ticket_cards/TicketContainer";
-import { getTicket } from "../../data/tickets_data/DataTickets";
+import { getTicket } from "../../data/database/trips.data";
 import { filterd_TripData } from "../../data/tickets_data/DataTickets";
+import { useEffect, useState } from "react";
+
+async function loadData() {
+    let retpromise = await getTicket(1,2,0,2,new Date(1401, 9, 29, 11, 23, 0),new Date(1401, 9, 30, 12, 2, 34)).then()
+    return retpromise;
+}
 
 export default function Home() {
-    let filteredData : filterd_TripData[] = [
-        {
-            departure_date: new Date(1401, 3, 24, 18, 26),
-            company_name: 'چابک سواران',
-            company_Score: 4.4,
-            company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-            trip_des: ['ماهانVIP(مانیتورشخصی)'],
-            start_point_city: 'اصفهان',
-            destination_city: 'تهران',
-            Remaining_seats: 10,
-            price: 1220000,
-        },
-        {
-            departure_date: new Date(1401, 5, 3, 6, 2),
-            company_name: 'چابک سواران',
-            company_Score: 4.4,
-            company_image: 'https://cdn.alibaba.ir/static/img/bus/HMSFR.jpg',
-            trip_des: ['ماهانVIP(مانیتورشخصی)','اکونومی'],
-            start_point_city: 'اصفهان',
-            destination_city: 'تهران',
-            receive_date : new Date(1401, 5, 25, 17, 20),
-            Remaining_seats: 10,
-            price: 1320000
-        },
-    ]
+    const [tickets,setTickets] = useState<filterd_TripData[]>([]);
+    useEffect(() => {
+        const fetchData = async () => {
+          const data = await getTicket(1,2,0,2,new Date(1401, 9, 29, 11, 23, 0),new Date(1401, 9, 30, 12, 2, 34)).then();
+          setTickets(data);
+        }
+        fetchData()
+          .catch(console.error);;
+      }, [])
+
     return (
-        <Grid display={'flex'} direction={'row'} justifyContent={'center'} alignItems='center' width={'100vw'} height={'100vh'}>
+        <Grid container display={'flex'} direction={'row'} justifyContent={'center'} alignItems='center' width={'100vw'} height={'100vh'}>
             <Grid width={'1000px'} padding='10px'>
-                <TicketContainer filteredData={filteredData} tripType={1}/>
+                <TicketContainer filteredData={tickets} tripType={1}/>
             </Grid>
         </Grid>
     )
