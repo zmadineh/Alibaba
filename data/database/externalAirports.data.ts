55c179aacca0b10fbc2ddbe6e92265e4caef12ab
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-30 15:56:21 +0330

    feat database

--- data/database/externalAirports.data.ts
+++ data/database/externalAirports.data.ts
@@ -1,12 +1,13 @@
-import {data} from "../../model/data.type";
-import {startDesPointType} from "../../model/database/startDesPoint.typ";
+import { data } from "../../model/data.type";
+import { startDesPointType } from "../../model/database/startDesPoint.typ";
 
-export const externalAirports : startDesPointType[] = [
-    {id: 1, name: 'استانبول همه فرودگاه ها', city_id: 9, type: 'airport'},
-    {id: 2, name: 'دبی همه فرودگاه ها', city_id: 17, type: 'airport'},
-    {id: 3, name: 'ایروان همه فرودگاه ها', city_id: 11, type: 'airport'},
-    {id: 4, name: 'تورنتو همه فرودگاه ها', city_id: 18, type: 'airport'},
-    {id: 5, name: 'فرانکوفرت همه فرودگاه ها', city_id: 15, type: 'airport'},
-    {id: 6, name: 'میلان همه فرودگاه ها', city_id: 20, type: 'airport'},
-    {id: 7, name: 'لندن همه فرودگاه ها', city_id: 19, type: 'airport'},
+export const externalAirports: startDesPointType[] = [
+    { id: 1, name: 'استانبول همه فرودگاه ها', city_id: 9, type: 'airport' },
+    { id: 2, name: 'دبی همه فرودگاه ها', city_id: 17, type: 'airport' },
+    { id: 3, name: 'ایروان همه فرودگاه ها', city_id: 11, type: 'airport' },
+    { id: 4, name: 'تورنتو همه فرودگاه ها', city_id: 18, type: 'airport' },
+    { id: 5, name: 'فرانکوفرت همه فرودگاه ها', city_id: 15, type: 'airport' },
+    { id: 6, name: 'میلان همه فرودگاه ها', city_id: 20, type: 'airport' },
+    { id: 7, name: 'لندن همه فرودگاه ها', city_id: 19, type: 'airport' },
+    { id: 8, name: ' فرودگاه  بین المللی امام خمینی', city_id: 21, type: 'airport' },
 ]
