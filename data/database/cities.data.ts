8ac72f83d004f2d69e68d447c9795d46b35bb0ce
maryamkaramad <maryamkaramad1373@gmail.com>
2022-12-30 13:11:44 +0330

    change database

--- data/database/cities.data.ts
+++ data/database/cities.data.ts
@@ -1,15 +1,21 @@
 export const cities = [
-    {id: 1, country_id: 1, name: 'تهران'},
-    {id: 2, country_id: 1, name: 'اصفهان'},
+    { id: 1, country_id: 1, name: 'تهران' },
+    { id: 2, country_id: 1, name: 'اصفهان' },
+    { id: 3, country_id: 1, name: 'تبریز' },
+    { id: 4, country_id: 1, name: 'شیراز' },
 
-    {id: 3, country_id: 2, name: 'استانبول'},
-    {id: 4, country_id: 2, name: 'ازمیر'},
+    { id: 5, country_id: 2, name: 'استانبول' },
+    { id: 6, country_id: 2, name: 'ازمیر' },
+    { id: 7, country_id: 2, name: 'دبی' },
+    { id: 8, country_id: 2, name: 'لندن' },
 
-    {id: 5, country_id: 3, name: 'ایروان'},
-    {id: 6, country_id: 3, name: 'گیومری'},
 
-    {id: 7, country_id: 4, name: 'برلین'},
-    {id: 8, country_id: 4, name: 'مونیخ'},
 
-    {id: 9, country_id: 5, name: 'پاریس'},
+    { id: 9, country_id: 3, name: 'ایروان' },
+    { id: 10, country_id: 3, name: 'گیومری' },
+
+    { id: 11, country_id: 4, name: 'برلین' },
+    { id: 12, country_id: 4, name: 'مونیخ' },
+
+    { id: 13, country_id: 5, name: 'پاریس' },
 ];
