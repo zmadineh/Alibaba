642972b5c78e1e49f33d017e3bdf336675090caf
nima babaei <nimani1378@gmail.com>
2022-12-30 13:47:51 +0330

    initial component step2 of confirmation

---
+++ components/confirmation-page/Information.tsx
@@ -0,0 +1,14 @@
+
+import { Grid } from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+
+
+interface formType{
+    name : string,
+    lastName : string,
+    
+}
+
+export default function Information(props: { res: boolean,forms:formType[]}) {
+    
+}
